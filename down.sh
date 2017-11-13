476f09e7f9c65e7f8595398dbb88daa37d24d6f6
Pritam Kadam <phkadam2008@gmail.com>
2017-11-13 12:17:00 +0530

    Add csw-jenkins-persistent template.

--- down.sh
+++ down.sh
@@ -1,10 +1,5 @@
 #!/usr/bin/env bash
 
-oc delete -f volumes/coursier_pv.yaml  
-oc delete -f volumes/ivy2_pv.yaml  
-oc delete -f volumes/ws_pv.yaml  
-oc delete -f volumes/coursier_pvc.yaml  
-oc delete -f volumes/ivy2_pvc.yaml  
-oc delete -f volumes/ws_pvc.yaml 
+oc delete -f volumes/
 
 oc delete project cicd
--- up.sh
+++ up.sh
@@ -4,11 +4,10 @@ oc new-project cicd --display-name="CI/CD"
 
 oc policy add-role-to-user edit system:serviceaccount:cicd:jenkins -n cicd
 
-oc create -f volumes/coursier_pv.yaml  
-oc create -f volumes/ivy2_pv.yaml  
-oc create -f volumes/ws_pv.yaml  
-oc create -f volumes/coursier_pvc.yaml  
-oc create -f volumes/ivy2_pvc.yaml  
-oc create -f volumes/ws_pvc.yaml 
+oc create -f volumes/
+
+oc create -f csw-jenkins-persistent -n openshift
+
+oc process csw-jenkins-persistent -n openshift | oc craete -f - -n cicd
 
 oc process -f jenkins.yml | oc create -f - -n cicd
