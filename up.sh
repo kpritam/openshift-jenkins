38865bd48fdffb1c49dcd2758fa081f9179ee2cf
Pritam Kadam <phkadam2008@gmail.com>
2017-11-13 14:35:59 +0530

    Update scripts

--- up.sh
+++ up.sh
@@ -8,6 +8,6 @@ oc create -f volumes/
 
 oc create -f csw-jenkins-persistent.yml -n openshift
 
-oc process csw-jenkins-persistent -n openshift | oc craete -f - -n cicd
+oc process csw-jenkins-persistent -n openshift | oc create -f - -n cicd
 
 oc process -f jenkins.yml | oc create -f - -n cicd
